fee34aacc04df27227117140593e2a6b864a38ae
Nattawut Phetmak <neizod@gmail.com>
2012-04-20 02:19:17 +0700

    fix replacing inside quotation

---
+++ eg/dont-translate-me.py
@@ -0,0 +1,9 @@
+print "i know that pi (greek notation: π) = 3.1416..." 
+print 'i also know that integer can be written by this symbol: ℤ' 
+print "thefore, π", "∈" if ℤ(π) == π else "∉", "ℤ."
+
+print """
+lambda (λ) expression is not that hard!
+    first, we said that "λ x: x" is an indentical function.
+    then, "λ x: y" is a constant function (which will return 'y' at any cases).
+    last, "λ x: f(x)" is any function that return f(x) as a result."""
